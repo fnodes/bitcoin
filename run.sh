413d60b5e91af6dfa76d4d903991b77a1bed58e6
Tyler Baker <tyler@foundries.io>
2019-01-03 19:12:52 -0800

    Add bitcoind helper
    Signed-off-by: Tyler Baker <tyler@foundries.io>

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker rm bitcoind
 docker volume create bitcoin
 docker run -dit --name bitcoind --net=host --restart=always -e 'BTC_RPCUSER=admin' -e 'BTC_RPCPASSWORD=changeme' -e 'BTC_TXINDEX=1' -e 'BTC_DISABLE_WALLET=0' -v bitcoin:/root/.bitcoin fullnodes/bitcoin:latest
 mkdir -p ~/.bin
-cp $pwd/bin/bitcoin-cli ~/.bin
+cp $(pwd)/bin/bitcoin* ~/.bin
